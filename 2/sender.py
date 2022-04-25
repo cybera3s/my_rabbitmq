4522c1475af6a181fb41423183ad3d3fb600cf92
ario <cybera.3s@gmail.com>
2022-04-25 20:25:38 +0430

    create 2/sender.py

--- 2/sender.py
+++ 2/sender.py
@@ -7,7 +7,7 @@ ch1.queue_declare(queue='first', durable=True)
 message = 'Testing message'
 
 ch1.basic_publish(exchange='', routing_key='first', body=message,
-                  properties=pika.BasicProperties(delivery_mode=2))
+                  properties=pika.BasicProperties(delivery_mode=2, headers={'name': 'ario'}))  # ->  expiration='2000'
 
 print('message sent')
 connection.close()
