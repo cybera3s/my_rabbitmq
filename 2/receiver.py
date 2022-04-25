d035c65675e49d93a76a19f82e62315735afe435
ario <cybera.3s@gmail.com>
2022-04-25 17:48:07 +0430

    create 2/receiver.py

---
+++ 2/receiver.py
@@ -0,0 +1,20 @@
+import pika
+import time
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+ch = connection.channel()
+
+ch.queue_declare(queue='first', durable=True)
+print('Waiting for message, press ctrl+c to exit')
+
+
+def callback(ch, method, properties, body):
+    print(f"Received Body: {body}")
+    time.sleep(9)
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+ch.basic_qos(prefetch_count=1)
+ch.basic_consume(queue='first', on_message_callback=callback)
+
+ch.start_consuming()
